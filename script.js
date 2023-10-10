49e2a4033a1949a90364b471042bd2c550feab59
Bryan Middleton <bryanmiddleton1986@gmail.com>
2023-10-10 14:18:33 +0100

    validation to modal form

--- script.js
+++ script.js
@@ -18,47 +18,58 @@ function Book(title, author, pages, read){
   const confirmBtn = inputForm.querySelector("#confirmBtn");
   const cancelBtn = inputForm.querySelector("#cancelBtn");
   let buttonPressed = "";
-  let title = "";
-  let author = "";
-  let pages = "";
-  let read = "";
 
   //open modal form when buton is clicked
 
   addBookButton.addEventListener("click", ()=>{
     inputForm.showModal();
   });
-//set title based on input
-  titleInput.addEventListener("input", (e)=>{
-    title = e.target.value;
-    console.log(title);
-  });
-//set author based on input
-  authorInput.addEventListener("input", (e)=>{
-    author = e.target.value;
-    console.log(author);
-  });
-//set pages based on input
-  pagesInput.addEventListener("input", (e)=>{
-    pages = e.target.value;
-    console.log(pages);
-  });
-//set read based on drop down selection
-  readSelect.addEventListener("change", (e)=>{
-    read = e.target.value;
-    console.log(read);
-  });
+
 
   inputForm.addEventListener("close", (e) =>{
-    buttonPressed == "cancel" || title == "" || author == "" || pages == "" || read == "default"
+    //if pressing cancel, does nothing and displays current library
+    buttonPressed == "cancel"
     ? displayLibrary(myLibrary)//do nothing
-    : addBookToLibrary(title,author,pages,read);
+    : addBookToLibrary(titleInput.value,authorInput.value,pagesInput.value,readSelect.value);
+    console.table(titleInput.value,authorInput.value,pagesInput.value,readSelect.value);
     document.getElementById("modalForm").reset(); //resets form for next use
   });
 
-confirmBtn.addEventListener("click", (event) =>{
-   event.preventDefault();
-    inputForm.close(title,author,pages,read);
+
+confirmBtn.addEventListener("click", (event) =>{ 
+  
+  //check for valid inputs before allowing form to submit
+  if(titleInput.validity.valueMissing || authorInput.validity.valueMissing || pagesInput.validity.valueMissing || readSelect.value == "default"){
+  if(titleInput.validity.valueMissing){
+    titleInput.setCustomValidity("Title cannot be blank");
+  }
+  else{
+    titleInput.setCustomValidity("");
+  }
+  if(authorInput.validity.valueMissing){
+    authorInput.setCustomValidity("Author cannot be blank");
+  }
+  else{
+    authorInput.setCustomValidity("");
+  }
+  if(pagesInput.validity.valueMissing){
+    pagesInput.setCustomValidity("Pages cannot be blank");
+  }
+  else{
+    pagesInput.setCustomValidity("");
+  }
+  if(readSelect.value == "default"){
+    readSelect.setCustomValidity("Must select yes or no");
+  }
+  else{
+    readSelect.setCustomValidity("");
+    }
+  }  
+  else{
+    event.preventDefault();
+    //transfer values to close event
+    inputForm.close(titleInput.value,authorInput.value,pagesInput.value,readSelect.value);
+  }
  });
 
  cancelBtn.addEventListener("click", (event) =>{
@@ -68,7 +79,21 @@ confirmBtn.addEventListener("click", (event) =>{
 });
 
  
+function validate(input){
+  const validityState = input.validity;
 
+  if (validityState.valueMissing) {
+    input.setCustomValidity("You gotta fill this out, yo!");
+  } else if (validityState.rangeUnderflow) {
+    input.setCustomValidity("We need a higher number!");
+  } else if (validityState.rangeOverflow) {
+    input.setCustomValidity("That's too high!");
+  } else {
+    input.setCustomValidity("");
+  }
+
+  input.reportValidity();
+}
 
 function addBookToLibrary(title,author,pages,read){
     const newBook = new Book(title,author,pages,read);
